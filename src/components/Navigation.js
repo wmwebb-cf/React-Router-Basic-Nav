356f18c9d0f2a211e4e3cf330e1bd4b4eeb68e2f
William Webb <webbdevvue@gmail.com>
2020-03-11 20:38:10 -0400

    removed un-nedded imports from files, still works

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import { Home, About, Contact } from '/';
 
 const Navigation = () => {
